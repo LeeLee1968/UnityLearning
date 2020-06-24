a6b45632db2b59aaf7e0ac54cbbdb041d1cd4756
Lee <leanh1961998@gmail.com>
2020-06-24 15:39:03 +0700

    Commit

---
+++ leanh-game-2-prj/Assets/Scenes/CammeraController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CammeraController : MonoBehaviour
+{
+    public GameObject cube;
+    
+    private void Update()
+    {
+        //Tạo hàm raycast : lấy thông tin 
+        RaycastHit hit;
+        //Nếu giữ chuột 
+        if (Input.GetButton("Fire1"))
+        {
+            //Tạo tia từ Cammera đến điểm dc chọn (vị trí chuột)
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //Check xem tia vừa tạo có cắt vật thể nào không 
+            //Physics.Raycast(tia (ray) , điểm va chạm (out hit), khoảng cách lớn nhất (1000) )
+            if (Physics.Raycast(ray, out hit, 1000 ))
+            {
+                if (hit.collider.gameObject.tag == "Cube")
+                {
+                    Debug.DrawLine(ray.origin, hit.point);
+                    Debug.Log(hit.point);
+                    cube.transform.position = new Vector3(hit.point.x, 0.5f, hit.point.z);
+                }
+      
+            }
+          
+        }
+        
+    }
+}
+
+
+   
+
+
---
+++ leanh-game-2-prj/Assets/Scenes/MouseHover.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHover : MonoBehaviour
+{
+    public Color startColor;
+    public Color hoverColor;
+    private Vector3 mOffset;
+
+    //
+    bool mouseHover = false;
+    private void OnMouseDrag()
+    {
+       
+    }
+
+    private void OnMouseEnter()
+    {
+        mouseHover = true;
+        GetComponent<Renderer>().material.SetColor("_Color",hoverColor);
+    }
+    private void OnMouseExit()
+    {
+        mouseHover = false;
+        GetComponent<Renderer>().material.SetColor("_Color", startColor);
+    }
+   
+}
