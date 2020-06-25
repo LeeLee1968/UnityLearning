99615216ba49673499ea3530493c6c7834f5879e
Lee <leanh1961998@gmail.com>
2020-06-25 17:29:29 +0700

    new

---
+++ leanh-game-2-prj/Assets/Script/CameraController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject cube1;
+    RaycastHit hit;
+    public LayerMask layer;
+    public Rigidbody rb;
+    Vector3 cubePos;
+    void Start()
+    {
+        rb.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 1000))
+            {
+                Debug.DrawLine(ray.origin, hit.point);
+                if (hit.collider.gameObject.tag == "Cube2")
+                {
+                    cube1.transform.position = hit.transform.position - cube1.transform.position;
+                    //cube1.transform.Translate(hit.transform.position - cube1.transform.position);
+                    //rb.MovePosition(hit.transform.position - cube1.transform.position);
+                    //rb.velocity = (hit.transform.position - cube1.transform.position) * Time.deltaTime;
+                }
+                if (hit.collider.gameObject.tag == "Cube3")
+                {
+                    cube1.transform.position = hit.transform.position - cube1.transform.position;
+                    //cube1.transform.Translate(hit.transform.position - cube1.transform.position);
+                    //rb.MovePosition(hit.transform.position - cube1.transform.position);
+                    //rb.velocity = (hit.transform.position - cube1.transform.position) * Time.deltaTime;
+                }
+            }
+
+        }
+    }
+}
---
+++ leanh-game-2-prj/Assets/Script/Test.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        // Moves the GameObject using it's transform.
+        rb.isKinematic = true;
+    }
+
+    void FixedUpdate()
+    {
+        // Moves the GameObject to the left of the origin.
+        if (transform.position.x > 3.0f)
+        {
+            transform.position = new Vector3(-3.0f, 0.0f, 0.0f);
+        }
+
+        rb.MovePosition(transform.position + transform.right * Time.fixedDeltaTime);
+    }
+}
